cd3ef6e1fab1ae1d2971354a43811da491c50b6a
ogasawarayui <ogasawara.yui@outlook.com>
2023-12-17 00:47:21 +0900

    js7

---
+++ script.js
@@ -0,0 +1,23 @@
+/*Q1 AND(&&)*/
+var numA = 80;
+var numB = 60;
+if (numA == 80 && numB == 60) {
+  console.log("合格");
+} else {
+  console.log("不合格");
+};
+/*Q2 OR(||)*/
+var numA = 100;
+var numB = 80;
+if (numA == 100 || numB == 80) {
+  console.log("A");
+} else {
+  console.log("B");
+};
+/*Q3 NOT(!)*/
+var num = 0;
+if (!num) {
+  console.log("一致");
+} else {
+  console.log("不一致s");
+};
